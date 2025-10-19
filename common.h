042d0d1f8352f2ef0969bbbe525eb843cd378aa5
akshita-singhal24 <f20230712@hyderabad.bits-pilani.ac.in>
2025-10-19 12:25:39 +0530

    Update common.h

--- common.h
+++ common.h
@@ -1,12 +1,15 @@
+// common.h
 #ifndef COMMON_H
 #define COMMON_H
 
+
 typedef struct Node {
-    char *tok;           // token string: "~", "+", "*", ">", "x1", "x12", ...
+    char *tok; // This holds a STRING, not a char
     struct Node *left;
     struct Node *right;
 } Node;
 
+// Helper function prototypes
 Node* newNode(const char *tok);
 void freeTree(Node *r);
 char *strdup_s(const char *s);
